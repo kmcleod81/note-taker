8cb3f8af55a37297b5d787f352813d2bc7fa3f66
kdawg <mcleod.karla@gmail.com>
2020-06-13 17:14:10 -0400

    apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const notebook = require('../db/Notebook');
 // require express
 
 // require the class file so you can use the functions you created
@@ -8,19 +8,31 @@ const router = require('express').Router();
 // call the getNotes() from the class you required
 // res.json(note)
 
+router.get('/notes', function (req, res) {
+    notebook.getNotes()
+        .then((notes) => res.json(notes))
+        .catch((err) => res.status(500).json(err));
+});
+
 // POST request for /api/notes
 // call the addNotes() from the class you required
 // res.json(note)
+
+router.post('/notes', function (req, res) {
+    notebook.addNotes(req.body)
+        .then((note) => res.json(note))
+        .catch((err) => res.status(500).json(err));
+});
 
 // DELETE request for /api/notes
 // call the addNotes() from the class you required
 // res.json(note)
 
-//Use this maybe for dry code?
-app.all('/secret', function (req, res, next) {
-    console.log('Accessing the secret section ...')
-    next() // pass control to the next handler
-})
+router.delete('/notes/:id', function (req, res) {
+    notebook.deleteNotes(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
+});
 
 // delete request for api/notes/:id
 // call the deleteNote() function
