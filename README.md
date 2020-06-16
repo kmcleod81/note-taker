7cdde8125bfdcc51d3295ab1294c8dfc97a545f9
kdawg <mcleod.karla@gmail.com>
2020-06-16 18:27:58 -0400

    added readme and trying heroku again

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # note-taker
+
+![Readme Generator](./assets/img/noteTaker.gif)
+
+## Table of Contents:
+* [Description](#Description)
+* [How to Install](#How-to-Install)
+* [Usage](#Usage)
+* [Contributors](#Contributors)
+* [Link to Page](#Link-to-Page)
+
+## Description
+This application creates notes using the CRUD method - Create, Read, Update & Delete
+
+## How to Install
+run: npm install in your terminal/bash, then node index
+
+## Usage
+You would use this to notes that are savable, editable and deletable.
+
+## Contributors
+* **Karla McLeod** - [kmcleod81](https://github.com/kmcleod81)
+
+## Link to Page
+* https://kmcleod81.github.io/node-readme-generator/
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,9 @@
-const router = require('express').Router();
-const notebook = require('../db/Notebook');
+const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+const db = path.join(__dirname, "../db/db.json");
+
 // require express
 
 // require the class file so you can use the functions you created
@@ -8,31 +12,62 @@ const notebook = require('../db/Notebook');
 // call the getNotes() from the class you required
 // res.json(note)
 
-router.get('/notes', function (req, res) {
-    notebook.getNotes()
-        .then((notes) => res.json(notes))
-        .catch((err) => res.status(500).json(err));
+router.get("/notes", (req, res) => {
+    fs.readFile(db, "utf8", (err, data) => {
+        res.json(JSON.parse(data));
+        console.log("Notes Returned");
+    });
 });
 
 // POST request for /api/notes
 // call the addNotes() from the class you required
 // res.json(note)
 
-router.post('/notes', function (req, res) {
-    notebook.addNotes(req.body)
-        .then((note) => res.json(note))
-        .catch((err) => res.status(500).json(err));
+router.post("/notes", (req, res) => {
+    let newNote = req.body;
+    // assigning id to the new note using .uuidv4() 
+    newNote.id = uuidv4();
+
+    fs.readFile(db, "utf8", (err, data) => {
+        let note = JSON.parse(data);
+        note.push(newNote);
+
+        fs.writeFile(db, JSON.stringify(note), (err) => {
+            if (err) {
+                throw (err);
+
+            }
+            console.log("Note Recorded");
+        });
+    })
+    res.json(newNote);
 });
 
 // DELETE request for /api/notes
 // call the addNotes() from the class you required
 // res.json(note)
 
-router.delete('/notes/:id', function (req, res) {
-    notebook.deleteNotes(req.params.id)
-        .then(() => res.json({ ok: true }))
-        .catch((err) => res.status(500).json(err));
+router.delete("/notes/:id", (req, res) => {
+
+    fs.readFile(db, "utf8", (err, data) => {
+        let noteId = req.params.id;
+        let noteData = JSON.parse(data);
+        noteData = noteData.filter((note) => {
+            if (noteId != note.id) {
+                return true;
+            } else {
+                return false;
+            };
+        });
+        // then rewrite notes to db file
+        fs.writeFile(db, JSON.stringify(noteData), function (err) {
+            if (err)
+                throw err;
+            res.send(console.log("Note Deleted"));
+        })
+    });
 });
+
 
 // delete request for api/notes/:id
 // call the deleteNote() function
